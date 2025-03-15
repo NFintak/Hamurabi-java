31497bc33200c8c15d38818f5562cd3e0d210b27
Nathan <natefintak@gmail.com>
2025-03-15 19:46:01 -0400

    restarted project

---
+++ Hammurabi.java
@@ -0,0 +1,21 @@
+package hammurabi;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hammurabi {
+    Random rand = new Random();
+    Scanner scan = new Scanner(System.in);
+
+
+
+
+    public static void main(String[] args) {
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+
+
+    }
+
+}
